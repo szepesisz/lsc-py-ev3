ec30dbe23f987f3f750e8b6f967cae22360de025
flora <floraorban@gmail.com>
2024-09-20 11:34:53 +0200

    INIT

---
+++ task_1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
+import sys
+
+
+print(sys.version)
+
+# Create your objects here
+
+# Initialize the EV3 Brick.
+ev3 = EV3Brick()
+
+# Initialize a motor at port B.
+test_motor = Motor(Port.B)
+
+# Write your program here
+
+# Play a sound.
+ev3.speaker.beep()
+
+# Run the motor up to 500 degrees per second. To a target angle of 90 degrees.
+test_motor.run_target(500, 360)
+
+# Play another beep sound.
